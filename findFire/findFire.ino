e63c7e434b89d365567909b8b13a5894a8e73972
MattWelc01 <79120136+MattWelc01@users.noreply.github.com>
2023-05-12 14:18:08 +1200

    removed old files

---
+++ findFire/findFire.ino
@@ -0,0 +1,18 @@
+  int IRpin = A13;
+
+void setup() {
+  pinMode(14, OUTPUT);
+  pinMode(IRpin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  /*
+  digitalWrite(14,HIGH);
+  delay(500);
+  digitalWrite(14,LOW);
+   delay(500);
+   */
+   Serial.println(analogRead(IRpin));
+   delay(100);
+}
