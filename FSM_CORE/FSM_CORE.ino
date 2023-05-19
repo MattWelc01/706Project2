b85e6046232b4346a13e5628e1f8c69356f1a3df
MattWelc01 <79120136+MattWelc01@users.noreply.github.com>
2023-05-19 16:04:29 +1200

    Added states and transition conditionals
    To add state routine functions

--- FSM_CORE/FSM_CORE.ino
+++ FSM_CORE/FSM_CORE.ino
@@ -1,7 +1,88 @@
+int FIRE_FLAG = 0;
+int OBSTACLE_FLAG = 0;
+
+
+enum State {
+  STATE_A,
+  STATE_B,
+  STATE_C,
+  STATE_D,
+  STATE_E
+};
+
+
+State currentState = STATE_A;
+State previousState = STATE_A;
+
 void setup(){
   
 }
 
 void loop(){
+  previousState = currentState;// THIS LOGIC MAY NOT BE CORRECT, DOUBLE CHECK LATER
   
+  // The switch-case statement represents the FSM
+  switch(currentState) {
+    case STATE_A:
+      
+
+      //FUNCTIONS AND SHIT GO HERE
+      
+      if (OBSTACLE_FLAG) { 
+        currentState = STATE_E;
+      }
+      if (!OBSTACLE_FLAG && FIRE_FLAG) { 
+        currentState = STATE_C;
+      }else{
+        currentState = STATE_B;
+      }
+      
+      break;
+
+    case STATE_B:
+      
+      if (OBSTACLE_FLAG) { 
+        currentState = STATE_E;
+      }
+      if (!OBSTACLE_FLAG && FIRE_FLAG) { 
+        currentState = STATE_C;
+      }else{
+        currentState = STATE_A;
+      }
+      break;
+
+    case STATE_C:
+      
+      // Transition to the next state
+      if (!FIRE_FLAG && OBSTACLE_FLAG) {
+        currentState = STATE_E;
+      }
+      if(FIRE_FLAG && !OBSTACLE_FLAG){
+        currentState = STATE_C;
+      }
+      if(FIRE_FLAG && OBSTACLE_FLAG){
+        currentState = STATE_D;
+      }else{
+        currentState = STATE_B;
+      }
+      break;
+
+
+      case STATE_D:
+      
+      // Transition to the next state
+      if (FIRE_FLAG) {
+        currentState = STATE_D;
+      }else{
+        currentState = STATE_A;
+      }
+      break;
+
+
+      case STATE_E:
+     
+      // Transition to the next state
+      currentState = previousState;    
+      break;
+  }
 }
