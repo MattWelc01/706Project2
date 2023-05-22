bfd69b99122660393ff14d069c6af037e029b1e4
MattWelc01 <79120136+MattWelc01@users.noreply.github.com>
2023-05-22 17:20:00 +1200

    added avoidance logic

--- obstacleAvoid/obstacleAvoid.ino
+++ obstacleAvoid/obstacleAvoid.ino
@@ -7,3 +7,42 @@ void setup(){
 void loop(){
   
 }
+
+void obstacleAvoid(void){
+  if(SONAR_FLAG){
+    if(!IRLS && (IRLF + IRRF == 1)){
+      //strafe left
+    }else if(!IRRS && (IRLF + IRRF == 1)){
+      //strafe right
+    }else if(((IRLS && IRRS) && (IRLF || IRRF)){
+      //escape
+    }else if(IRLF && IRRF){
+      //find max dist
+    }else{
+      //escape THIS IS NOT MOST EFFICIENT PATH
+    }
+
+    
+  }
+  
+  if(!SONAR_FLAG){
+    if(!IRLS && (IRRF || IRLF)){
+      //STRAFE LEFT
+    }
+
+    if(!IRRS && (IRRF || IRLF)){
+      //STRAFE RIGHT
+    }
+    
+    if(IRLS && IRRS && (IRLF || IRRF)){
+      //ESCAPE
+    }
+
+    if(IRLS && IRRS && !(IRLF || IRRF)){
+      //SRAFE MIN DIST
+    }
+    if(!IRLF && !IRRF)){
+      //DRIVE FORWARD OPEN LOOP A BIT
+    }
+  }
+}
